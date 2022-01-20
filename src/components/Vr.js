ae4d29c6273d6c37bf67442ad59a6ede675f0ea5
sochi2122 <90932673+sochi2122@users.noreply.github.com>
2022-01-20 13:33:05 -0500

    VR Added

---
+++ src/components/Vr.js
@@ -0,0 +1,36 @@
+import React from "react"
+import '../index.css';
+
+
+const Vr = (props) =>{
+
+
+const tour = [ {
+
+
+
+    
+    
+    }]
+
+    return(
+
+
+    
+    
+    <div className="zillow">
+    <center>
+       
+
+
+         <iframe title="zillow"  src="https://www.zillow.com/view-3d-home/5feb0917-a442-4139-8354-9ca1bae0bce1" height="405px" width="850px" frameBorder="2px" allowFullScreen></iframe>
+
+       </center>
+       </div>
+       
+    )}
+
+
+export default Vr;
+
+
